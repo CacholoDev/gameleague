80e5f350aff4527d0da9c2caac9f3ef18b1a0018
CacholoDev <adriannoia104@gmail.com>
2026-04-17 22:03:53 +0200

    starting

---
+++ gameleague/src/main/java/com/gameleague/GameleagueApplication.java
@@ -0,0 +1,13 @@
+package com.gameleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameleagueApplication.class, args);
+	}
+
+}
---
+++ gameleague/src/test/java/com/gameleague/GameleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
